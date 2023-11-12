90cea08d15b0feca4ab9629bae057d75e9321844
Sloth-iNkt <ciavelm18@gmail.com>
2023-11-12 23:45:28 +0800

    Add readme file

--- src/com/oop/aquafarm/states/TitleState.java
+++ src/com/oop/aquafarm/states/TitleState.java
@@ -5,10 +5,14 @@ import com.oop.aquafarm.graphics.CFont;
 import com.oop.aquafarm.util.KeyHandler;
 import com.oop.aquafarm.util.MouseHandler;
 
+import javax.imageio.ImageIO;
 import java.awt.Graphics2D;
 import java.awt.Image;
 import java.awt.Color;
 import java.awt.Toolkit;
+import java.awt.image.BufferedImage;
+import java.io.File;
+import java.io.IOException;
 
 
 public class TitleState extends GameState {
@@ -35,9 +39,22 @@ public class TitleState extends GameState {
 
     @Override
     public void render(Graphics2D g) {
+//        BufferedImage backgroundgif = new BufferedImage(10,10, BufferedImage.TYPE_INT_ARGB);
+//        backgroundgif.createGraphics();
+//        g.setColor(Color.YELLOW);/*from   ww  w . j a v a  2s.com*/
+//        g.fillRect(0, 0, GamePanel.width, GamePanel.height);
+//        g.setColor(Color.BLACK);
+//        g.drawRect(1, 1, 6, 6);
+        BufferedImage backgroundgif = null;
+        try {
+            backgroundgif = ImageIO.read(new File( "res/background/background.gif"));
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+        g.drawImage(backgroundgif,0,0,null);
 
-        g.setColor(Color.BLACK);
-        g.fillRect(0,0, GamePanel.width, GamePanel.height);
+
+
         CFont fps = new CFont(Color.WHITE, "res/font/pixelated.ttf", "pixelated", 24, GamePanel.width / 2, 32);
         fps.drawString(g, "title");
     }
